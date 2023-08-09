20827c16cc7c4b747d764ae9a20925f6099b65c1
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2023-08-09 15:05:23 +0600

    First Commit

---
+++ app/Filament/Resources/CustomersResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomersResource\Pages;
+use App\Filament\Resources\CustomersResource\RelationManagers;
+use App\Models\Customers;
+use Filament\Forms;
+// use Filament\Forms\Components\FileUpload;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\Layout;
+
+class CustomersResource extends Resource
+{
+    protected static ?string $model = Customers::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')->email()->required(),
+                Forms\Components\FileUpload::make('image')->required()->directory('Images'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\ImageColumn::make('image'),
+            ])
+            ->filters([
+                Filter::make('name')
+                    ->query(fn (Builder $query): Builder => $query->where('name', true))
+
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomers::route('/create'),
+            'edit' => Pages\EditCustomers::route('/{record}/edit'),
+        ];
+    }
+
+    protected function getTableFiltersLayout(): ?string
+    {
+        return Layout::BelowContent;
+    }
+}
---
+++ app/Filament/Resources/CustomersResource/Pages/EditCustomers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CustomersResource\Pages;
+
+use App\Filament\Resources\CustomersResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCustomers extends EditRecord
+{
+    protected static string $resource = CustomersResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
